6207b0fb751220d28f412d502fc582f0cf2cdc8e
Luis Piñeres Rico <131925376+luisfepineres@users.noreply.github.com>
2023-06-03 01:30:25 +0200

    Add files via upload

---
+++ Read_Flux.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		std::cerr << "Usage: ./program <inputFile.shw>" << std::endl;
+		return 1;
+	}
+
+	std::string filePath = argv[1];
+	std::ifstream inputFile(filePath);
+		        
+	if (!inputFile) {
+		std::cerr << "Error opening input file: " << filePath << std::endl;
+		return 1;
+	}
+
+	std::string line;
+	int lineCount = 0;
+	while ((std::getline(inputFile, line))&& lineCount < 15 ) {
+	// Procesar cada línea del archivo aquí
+		std::cout << line << std::endl;
+		lineCount++;
+	}
+						
+	inputFile.close();
+	return 0;
+}
+						
